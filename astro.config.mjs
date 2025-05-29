7a10eb70c3a950475c143eb910c237af78c34989
JesdanRS <152462072+JesdanRS@users.noreply.github.com>
2025-05-29 14:14:08 -0400

    Fix SharedArrayBuffer and image path issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,13 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  devToolbar: { enabled: false }
+  devToolbar: { enabled: false },
+  vite: {
+    server: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp'
+      }
+    }
+  }
 });
